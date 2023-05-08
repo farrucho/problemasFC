054bd1ccaac561f9e67da43708b03551f16adb7a
farrucho <laranjofclash@gmail.com>
2023-05-08 15:39:27 +0100

    organizar

---
+++ Xico/Problemas/old/fdoublev.C
@@ -0,0 +1,5 @@
+double *fdoublev(int n){
+    double *v;
+    v = new double[n];
+    return v;
+}
---
+++ Xico/projeto/rImagem.C
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+
+int main()
+{
+    ifstream FI("./glassware_noisy.ascii.pgm");
+
+    if(FI.is_open()){
+        cout << "aberto ficheiro";   
+        while(FI)
+        {
+            string line;
+            getline(FI,line);
+            istringstream iss(line);
+
+            int nrows, ncols;
+            iss >> ncols >> nrows;
+
+
+            cout << nrows << " | " << ncols << endl;
+        }
+        
+    }else{
+        cout << "erro ao abrir ficheiro";
+        return -1;
+    }
+
+
+}

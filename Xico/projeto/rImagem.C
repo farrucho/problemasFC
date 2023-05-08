9006aff7586e979be85b7d56d366ba2a9aa27d36
farrucho <laranjofclash@gmail.com>
2023-05-08 17:35:21 +0100

    imagem para matriz implementada

--- Xico/projeto/rImagem.C
+++ Xico/projeto/rImagem.C
@@ -7,29 +7,68 @@
 using namespace std;
 
 
+/*
+ESTADO:
+bro por favor digam me como é que fizeram para converter em matriz
+o meu metodo ta bue ranhoso
+*/
+
+vector<int> line_2_vector(string line){
+    /*
+        Algoritmo para converter string line do tipo "1 2 3" para vector de int
+    */
+    vector<int> vec;
+    string stringnum;
+    for(int i = 0; i < line.size(); i++){
+        if(line[i] == ' ' || line[i] == '\n'){
+            vec.push_back(stoi(stringnum));
+            stringnum = "";
+        }else{
+            stringnum+=line[i];
+            if(i == line.size()-1){
+                vec.push_back(stoi(stringnum));
+                stringnum = "";
+            }
+        }
+    }
+    return vec;
+}
+
+
 int main()
 {
     ifstream FI("./glassware_noisy.ascii.pgm");
 
     if(FI.is_open()){
-        cout << "aberto ficheiro";   
-        while(FI)
-        {
-            string line;
-            getline(FI,line);
-            istringstream iss(line);
+        cout << "aberto ficheiro" << endl;
 
-            int nrows, ncols;
-            iss >> ncols >> nrows;
+        string line;
+        vector<vector<int>> imagemVec; //inicializar melhor isto, ta a alocar mt memoria
 
 
-            cout << nrows << " | " << ncols << endl;
+        getline(FI, line);//dar skip da primeira linha pois nao interessa e não é int para nao complicar
+        while(getline(FI, line)){ //enquanto nao chega ao final do ficheiro
+            imagemVec.push_back(line_2_vector(line));
         }
+
+        //teste para verificar se tem os pixeis certos
+        int counter = 0;
+        for(int i = 0; i < imagemVec.size(); i++){
+            for(int j = 0; j < imagemVec[i].size(); j++){
+                counter+=1;
+            }
+        }
+        cout << counter << " previsto: " << 3 + imagemVec[0][0]*imagemVec[0][1] << endl;
         
     }else{
         cout << "erro ao abrir ficheiro";
         return -1;
     }
 
+    /*string a ="12 34 56";
+    vector<int> v = line_2_vector(a);
 
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << endl;
+    }*/
 }

407fb8c9e4b5c7df03c67a601b527c89d24b740e
salvado04 <132922810+salvado04@users.noreply.github.com>
2023-05-11 10:20:09 +0100

    colorfreq salvado

---
+++ Salvado/ColourFreq.C
@@ -0,0 +1,44 @@
+#include "ColourFreq.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// Frequência absoluta 
+vector<int> f_abs (vector<vector<int>> V, int N)
+{
+    vector <int> ColourFreq(N+1);
+    fill(ColourFreq.begin(), ColourFreq.end(), 0);
+    for(int i = 0; i<V.size(); i++)
+    {
+        for(int j = 0; j<V[i].size(); j++)
+        {
+            int idx;
+            idx = V[i][j];
+            ColourFreq[idx] += 1;
+        }
+    }
+    return ColourFreq;
+}
+
+// Imprimir este vetor na main do final 
+
+// Frequência Relativa
+
+vector <double> f_rel(vector<int> V)
+{
+    int sum = 0;
+    for (int i; i < V.size(); i++)
+    {
+        sum += V[i];
+    }
+
+    vector <double> V_fr;
+    for (int j; j < V.size(); j++)
+    {
+        V_fr.push_back((V[j]/sum)*100);
+    }
+
+}
+
+// Imprimir este resultado também (Imprimir com printf %2 -> 2 casas decimais)
---
+++ Salvado/ColourFreq.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+#ifndef _COLOURFREQ_
+#define _COLOURFREQ_
+
+
+std::vector<int> f_abs(std::vector<std::vector<int>>, int);
+
+
+#endif
